d99c1852d4c922fbf9405f248d78769d132b43ed
Axel Mora <axelmora2011@axels-mbp.lan>
2022-09-21 21:59:19 -0700

    Implemented createGallery() test so far

--- BidMeAPI/src/test/java/BidMeAPI/DAO/GalleryDAOTests.java
+++ BidMeAPI/src/test/java/BidMeAPI/DAO/GalleryDAOTests.java
@@ -1,15 +1,31 @@
 package BidMeAPI.DAO;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.nio.file.Files;
+import java.io.File;
 
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class GalleryDAOTests {
 
+	GalleryDAOImpl galleryDAO = new GalleryDAOImpl();
+
+	int galleryID = 1;
+	int entryID = 0;
+	File fi = new File("toolssplash.jpg");
+	byte[] image = Files.readAllBytes(fi.toPath());
+
+	Gallery gallery = new Galllery(galleryID, entryID, image);
+
+
+
 	@Test
 	void testCreateGallery() {
 		System.out.println("testing create gallery...");
-		
+
+		galleryDAO.createGallery(gallery);
+
+		assertNotNull(galleryDAO.getGallery(galleryID));
 		
 	}
 	
